53539a1060b0177b0ebb269156b6b9ce411102c3
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2021-06-19 19:24:29 -0300

    Update decisoes.php

--- decisoes.php
+++ decisoes.php
@@ -1,19 +1,25 @@
 <?php
 
-$idade = 22;
+$idade = 18;
 
-$nome = 'Joao';
+
+$acompanhado = 1;
 
 echo "Você só pode entrar se tiver a partir de 18 anos." . PHP_EOL;
 
-if ($idade >= 18 && $nome = 'Joao'){
+if ($idade >= 18){
     echo "Você tem $idade anos". PHP_EOL;
     echo 'Pode entrar';
+}
+elseif($idade < 18 and $acompanhado == 1){
+    echo "Você é menor de idade, porem esta acompanhado". PHP_EOL;
+    echo "Pode entrar";
 }else{
     echo "Você é menor de idade. Entrada proibida"
     . PHP_EOL;
-    echo "apenas pessoas com $idade ou mais pode entrar"
+    echo "apenas pessoas com acompanhante ou maior de 18 anos pode entrar"
     . PHP_EOL;
-    echo "Entrada exclusiva para pessoas com o nome de $nome"
-    . PHP_EOL;
+    
 }
+echo "". PHP_EOL;
+echo "Adeus!";
