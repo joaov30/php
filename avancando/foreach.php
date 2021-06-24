92875ac15be37a21fc3ac50b1e6ad67ebfed2b9d
João Victor C Baptista <76636099+joaov30@users.noreply.github.com>
2021-06-24 19:55:14 -0300

    Update foreach.php

--- avancando/foreach.php
+++ avancando/foreach.php
@@ -1,20 +1,22 @@
 <?php
 
-$conta1 = [
-    'titular' => 'Victor',
-    'saldo' => 1000
-];
-$conta2 = [
-    'titular' => 'Mariza',
-    'saldo' => 10000
-];
 
-$conta3 = [
-    'titular' => 'Alan',
-    'saldo' => 300
-];
-$contasCorrentes = [$conta1, $conta2, $conta3];
+$contasCorrentes = [
+    12345678910 => [
+        'titular' => 'Victor',
+        'saldo' => 1000
+    ],
+    12345678911 => [
+        'titular' => 'Mariza',
+        'saldo' => 10000
+    ],
+    12345678912 => [
+        'titular' => 'Alan',
+        'saldo' => 300
+    ]
+    ];
 
-foreach($contasCorrentes as $conta) {
+foreach($contasCorrentes as $cpf => $conta) {
+    echo "$cpf: ";
     echo $conta ['titular'] . PHP_EOL;
 }
